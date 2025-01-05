ac8d8ecc53214e3b6d3f8393d141c179f3493e90
natenka <natasha.samoylenko@gmail.com>
2025-01-05 18:15:43 +0000

    UPD textual experiments. Docs guide

--- code_examples/pyneng_quiz_draft_rewrite/termquiz_app.py
+++ code_examples/pyneng_quiz_draft_rewrite/termquiz_app.py
@@ -4,6 +4,7 @@ from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalScroll
 from textual.reactive import reactive
 from textual.widgets import Button, Footer, Header, Static
+from textual import events
 
 
 def load_topics(questions_file):
@@ -16,12 +17,12 @@ topics = load_topics("questions.json")
 
 
 class TopicText(Button):
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        button_id = event.button.id
-        self.select()
+    #def on_button_pressed(self, event: Button.Pressed) -> None:
+    #    self.select()
 
     def select(self) -> None:
         self.add_class("selected")
+        self.label = self.id
 
 
 class Topics(VerticalScroll):
@@ -31,6 +32,15 @@ class Topics(VerticalScroll):
 class TermQuiz(App):
     CSS_PATH = "quiz_style.tcss"
 
+    #def compose(self) -> ComposeResult:
+    #    yield Header()
+    #    yield Footer()
+    #    topics_list = [
+    #        TopicText(f"{topic_id:<4} {text}", id=f"topic_{topic_id}")
+    #        for topic_id, text in enumerate(topics, 1)
+    #    ]
+    #    yield Topics(*topics_list, id="topics")
+
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
@@ -38,7 +48,17 @@ class TermQuiz(App):
             TopicText(f"{topic_id:<4} {text}", id=f"topic_{topic_id}")
             for topic_id, text in enumerate(topics, 1)
         ]
-        yield Topics(*topics_list, id="topics")
+        yield from topics_list
+
+    def on_key(self, event: events.Key) -> None:
+        if event.key.isdecimal():
+            selected_topic_text = self.query_one(f"#topic_{event.key}")
+            selected_topic_text.select()
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        event.button.select()
+        # selected_topic_text = self.query_one(f"#{event.button.id}")
+        # selected_topic_text.select()
 
 
 if __name__ == "__main__":
