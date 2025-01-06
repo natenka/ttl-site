2819a5ebb7e0964e7d682b69e67f781c0df49db5
natenka <natasha.samoylenko@gmail.com>
2025-01-06 14:17:32 +0000

    NEW textual guide practice. Events and messages

---
+++ code_examples/textual_ttl/guide_events_quiz_version_parent_widget.py
@@ -0,0 +1,58 @@
+import json
+
+from textual.app import App, ComposeResult
+from textual.containers import HorizontalGroup, VerticalScroll
+from textual.message import Message
+from textual.reactive import reactive
+from textual.widgets import Button, Footer, Header, Static
+from textual import events
+
+from rich import inspect
+
+def load_topics(questions_file):
+    with open(questions_file) as f:
+        q_dict = json.load(f)
+        return q_dict
+
+
+class TopicText(Static):
+    class Selected(Message):
+        def __init__(self, topic: str) -> None:
+            self.topic = topic
+            super().__init__()
+
+    def __init__(self, topic: str) -> None:
+        self.topic = topic
+        super().__init__()
+
+    def on_click(self) -> None:
+        self.post_message(self.Selected(self.topic))
+
+    def render(self) -> str:
+        return str(self.topic)
+
+
+class TermQuiz(App):
+    CSS_PATH = "quiz_style.tcss"
+    TOPICS = load_topics("questions.json")
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Static("Виберіть тему та натисніть Enter", id="intro")
+        topics_list = [TopicText(topic) for topic in self.TOPICS]
+        yield VerticalScroll(*topics_list, id="topics")
+
+    def on_topic_text_selected(self, message: TopicText.Selected) -> None:
+        selected_topic = message.topic
+        topics = self.query_one("#topics").remove()
+
+        new_topic = TopicText(selected_topic)
+        header = self.query_one("#intro")
+        header.mount(new_topic)
+        new_topic.styles.animate("background", "green", duration=0.1)
+
+
+if __name__ == "__main__":
+    app = TermQuiz()
+    app.run()
