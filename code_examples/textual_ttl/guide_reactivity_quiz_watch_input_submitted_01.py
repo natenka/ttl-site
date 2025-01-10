43e2e2410513fc26ad5d0cfe13f568eab3014f54
natenka <natasha.samoylenko@gmail.com>
2025-01-10 14:11:20 +0000

    UPD textual guide input quiz experiments

--- code_examples/textual_ttl/guide_reactivity_quiz_watch_input_submitted_01.py
+++ code_examples/textual_ttl/guide_reactivity_quiz_watch_input_submitted_01.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, VerticalScroll
@@ -8,6 +9,7 @@ from textual.widgets import Button, Footer, Header, Input, Static
 from textual import events
 
 from rich import inspect
+
 
 def load_topics(questions_file):
     with open(questions_file) as f:
@@ -33,7 +35,7 @@ class TopicText(Static):
 
 
 class TermQuiz(App):
-    CSS_PATH = "quiz_style.tcss"
+    CSS_PATH = "quiz_style_02.tcss"
     TOPICS = load_topics("questions.json")
 
     def compose(self) -> ComposeResult:
@@ -43,14 +45,28 @@ class TermQuiz(App):
         topics_list = [TopicText(topic) for topic in self.TOPICS]
         yield VerticalScroll(*topics_list, id="topics")
 
-    def on_topic_text_selected(self, message: TopicText.Selected) -> None:
-        selected_topic = message.topic
-        topics = self.query_one("#topics").remove()
+    def remount_topics(self, topics):
+        self.query_one("#topics").remove()
 
-        new_topic = TopicText(selected_topic)
-        header = self.query_one("#intro")
-        header.mount(new_topic)
-        new_topic.styles.animate("background", "green", duration=0.1)
+        topics_list = [TopicText(topic) for topic in topics]
+        # self.mount(VerticalScroll(*topics_list, id="topics"))
+        self.mount(VerticalScroll(*topics_list))
+
+    def highlight_selected_topic(self, selected_topic: str):
+        # self.query_one("#topics").remove()
+        # new_topic = TopicText(selected_topic)
+        # self.mount(new_topic)
+
+        self.remount_topics([selected_topic])
+        # new_topic.styles.animate("background", "green", duration=0.1)
+
+    def on_topic_text_selected(self, message: TopicText.Selected) -> None:
+        self.highlight_selected_topic(message.topic)
+
+    def on_input_submitted(self, event: Input.Submitted) -> None:
+        search_topic = event.value
+        matched_topics = ""
+        # self.query_one(Input).value = ""
 
 
 if __name__ == "__main__":
